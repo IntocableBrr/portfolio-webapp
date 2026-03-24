1089c4f54f0a94adad81e2d893260edc36139b44
Intocable Brr <jacobjames1117@gmail.com>
2026-03-24 17:03:16 -0500

    pfl-1 added base logic send form to api

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,51 +1,127 @@
 // Uncomment this line to use CSS modules
 // import styles from './app.module.scss';
-import NxWelcome from './nx-welcome';
 
 import { Route, Routes, Link } from 'react-router-dom';
+import { Button as MuiButton, ButtonProps, Avatar, TextField, Button, Alert, Box  } from '@mui/material';
+import { useState } from 'react';
 
 export function App() {
-  return (
-    <div>
-      <NxWelcome title="portfolio-webapp" />
+  const [email, setEmail] = useState('');
+  const [emailTouched, setEmailTouched] = useState(false);
+  const [message, setMessage] = useState('');
+  const [name, setName] = useState('');
+  const [messageTouched, setMessageTouched] = useState(false);
+  const [success, setSuccess] = useState('');
+  const [error, setError] = useState('');
 
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              This is the generated root route.{' '}
-              <Link to="/page-2">Click here for page 2.</Link>
-            </div>
-          }
-        />
-        <Route
-          path="/page-2"
-          element={
-            <div>
-              <Link to="/">Click here to go back to root page.</Link>
-            </div>
-          }
-        />
-      </Routes>
-      {/* END: routes */}
-    </div>
+  const validateEmail = (value: string) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(value);
+  };
+  
+  const isValid = message.trim().length >= 10;
+  const showErrorMessage = messageTouched && !isValid;
+  const showErrorEmail = emailTouched && !validateEmail(email);
+  const isError = showErrorMessage || showErrorEmail
+
+  const handleSubmit = async (e: React.SubmitEvent) => {
+    e.preventDefault();
+
+    setSuccess('');
+    setError('');
+
+    if (!isError) {
+      try {
+        const response = await fetch('http://localhost:5000/api/contact', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            name,
+            email,
+            message,
+          }),
+        });
+
+        const data = await response.json();
+
+        if (!response.ok) {
+          throw new Error(data.error || 'Something went wrong');
+        }
+
+        setSuccess('Message sent successfully!');
+
+        setEmail('');
+        setEmailTouched(false);
+        setName('');
+        setMessageTouched(false);
+        setMessage('');
+      } catch (err) {
+        setError('Something went wrong');
+      }
+    }
+  };
+      
+  return (
+    <>
+      {success && <Alert severity="success">{success}</Alert>}
+      {error && <Alert severity="error">{error}</Alert>}
+      <Box sx={{ justifyContent: "center", display: "flex"}}>
+        <Box 
+          component="form" 
+          onSubmit={handleSubmit}
+          sx={{ flexDirection: "column", display: "flex"}}
+        >
+          <Avatar 
+            alt='Jacob Rosa' 
+            src='jacob-avatar.jpg' 
+            sx={{ width: 250, height: 250}}
+          />
+          <TextField
+            fullWidth
+            label="Name"
+            name="name"
+            onChange={(e) => setName(e.target.value)}
+            sx={{ width: 250, marginTop: "20px"}}
+            value={name}
+          />
+          <TextField
+            error={showErrorEmail}
+            fullWidth
+            helperText={showErrorEmail ? 'Enter a valid email' : ''}
+            label="Email"
+            name="email"
+            onBlur={() => setEmailTouched(true)}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            sx={{ width: 250, marginTop: "20px"}}
+            type="email"
+            value={email}
+          />
+          <TextField
+            error={showErrorMessage}
+            fullWidth
+            helperText={showErrorMessage ? 'Message must be at least 10 characters' : ''}
+            label="Message"
+            multiline
+            onBlur={() => setMessageTouched(true)}
+            onChange={(e) => setMessage(e.target.value)}
+            required
+            rows={4}
+            sx={{ width: 250, marginTop: "20px"}}
+            value={message}
+          />
+          <Button 
+            sx={{ width: 250, marginTop: "20px"}}
+            type="submit" 
+            variant="contained"
+          >
+            Send
+          </Button>
+        </Box>
+      </Box>
+    </>
   );
 }
 

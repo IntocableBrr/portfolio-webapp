e976ca01fd8e3d6c4ae9a65185e5c67e5d958874
Intocable Brr <jacobjames1117@gmail.com>
2026-06-11 11:38:56 -0500

    PFL-10 added hexagon background

--- src/app/pages/home/home.tsx
+++ src/app/pages/home/home.tsx
@@ -3,6 +3,7 @@ import { Box } from '@mui/material';
 
 import Form from '../../components/form/form';
 import About from '../../components/about/about';
+import ShapeGrid from '../../components/shape-grid/ShapeGrid';
 
 type HomeProps = {
   title?: string;
@@ -10,18 +11,44 @@ type HomeProps = {
 };
 
 const HomePage: FC<HomeProps> = ({ title, subtitle }) => {
-
   return (
     <Box
       sx={{
+        position: 'relative',
         display: 'flex',
+        backgroundColor: '#000000',
         justifyContent: 'space-around',
         flexWrap: 'wrap',
         pt: 2,
+        minHeight: '100vh',
+        overflow: 'hidden',
       }}
     >
-      <About />
-      <Form />
+      <Box
+        sx={{
+          position: 'absolute',
+          inset: 0,
+          zIndex: 0,
+          width: '100%',
+          height: '100%',
+          pointerEvents: 'none',
+        }}
+      >
+        <ShapeGrid/>
+      </Box>
+      <Box
+        sx={{
+          position: 'relative',
+          zIndex: 1,
+          display: 'flex',
+          justifyContent: 'space-around',
+          flexWrap: 'wrap',
+          width: '100%',
+        }}
+      >
+        <About />
+        <Form />
+      </Box>
     </Box>
   );
 };
